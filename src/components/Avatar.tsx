e3565f50593de36153257c3a2268c570b52db817
Muhammad Rizqi <muhammad-rizqi@live.com>
2021-04-02 14:11:09 +0700

    add icon in avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,34 +1,71 @@
 import React, { FunctionComponent } from 'react';
-import { ColorValue, Image, ImageProps, View } from 'react-native';
+import { Image, ImageProps, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface AvatarProps extends ImageProps {
   size: 'xSmall' | 'small' | 'medium' | 'large' | number;
   notification?: boolean;
-  notificationColor?: ColorValue
+  notificationColor?: string;
+  buttonColor?: string;
+  iconColor?: string;
 }
 
-const sizeValue = { xSmall: 24, small: 48, medium: 72, large: 120 }
+const sizeValue = { xSmall: 24, small: 48, medium: 72, large: 120 };
+const iconSizeValue = { xSmall: 20, small: 20, medium: 24, large: 38 };
 
-const Avatar: FunctionComponent<AvatarProps> = ({ notification, notificationColor, size, style, ...props }) => {
+const Avatar: FunctionComponent<AvatarProps> = ({
+  notification,
+  notificationColor,
+  size = "medium",
+  style,
+  iconColor,
+  buttonColor,
+  ...props
+}) => {
   const passedProps = Array.isArray(props) ? Object.assign({}, ...props) : props;
   const passedStyles = Array.isArray(style) ? Object.assign({}, ...style) : style;
-  const imgSize = typeof size === 'number' ? size : sizeValue[size]
+  const imgSize = typeof size === 'number' ? size : sizeValue[size];
+  const iconSize = typeof size === 'number' ? 38 : iconSizeValue[size];
 
   return (
-    <View style={notification && {
-      padding: 4,
-      borderWidth: 2,
-      borderColor: notificationColor || '#000',
-      width: imgSize + 6,
-      height: imgSize + 6,
-      borderRadius: (imgSize + 6) / 2,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <Image
-        {...passedProps}
-        style={{ width: imgSize, height: imgSize, borderRadius: imgSize / 2, ...passedStyles }}
-      />
+    <View
+      style={{
+        position: 'relative',
+        width: imgSize + 6,
+        height: imgSize + 6,
+      }}>
+      <View
+        style={
+          notification && {
+            padding: 4,
+            borderWidth: 2,
+            borderColor: notificationColor || '#000',
+            width: imgSize + 6,
+            height: imgSize + 6,
+            borderRadius: (imgSize + 6) / 2,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }
+        }>
+        <Image
+          {...passedProps}
+          style={{ width: imgSize, height: imgSize, borderRadius: imgSize / 2, ...passedStyles }}
+        />
+      </View>
+      <TouchableOpacity
+        style={{
+          width: iconSize,
+          height: iconSize,
+          borderRadius: iconSize / 2,
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: buttonColor || '#17B582',
+        }}>
+        <Icon name="plus" color={iconColor || 'white'} size={iconSize / 2} />
+      </TouchableOpacity>
     </View>
   );
 };
--- src/screens/AvatarScreen.js
+++ src/screens/AvatarScreen.js
@@ -9,25 +9,36 @@ const AvatarScreen = () => {
         source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
         size="xSmall"
         notification
+        notificationColor="#00676D"
       />
       <Avatar
         source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
         size="xSmall"
         notification
+        notificationColor="#00676D"
       />
       <Avatar
         source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
         size="small"
+        notificationColor="#00676D"
         notification
       />
       <Avatar
         source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
         size="medium"
         notification
-        notificationColor="blue"
+        notificationColor="#00676D"
+        iconColor="white"
+        buttonColor="#17B582"
       />
-      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size="large" />
-      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size={30} />
+      <Avatar
+        source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
+        size="large"
+        notificationColor="#00676D"
+        iconColor="white"
+        buttonColor="#17B582"
+      />
+      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} />
     </View>
   );
 };
