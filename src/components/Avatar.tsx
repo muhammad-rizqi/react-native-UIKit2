60527ad78b699a867cd92650e171922660991c6c
Muhammad Rizqi <muhammad-rizqi@live.com>
2021-04-02 13:21:17 +0700

    change avatar notification

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,19 +1,43 @@
-import React, { FunctionComponent } from 'react'
-import { Image, ImageProps, View } from 'react-native'
+import React, { FunctionComponent } from 'react';
+import { ColorValue, Image, ImageProps, View } from 'react-native';
 
 interface AvatarProps extends ImageProps {
-  size: "xSmall" | "small" | "medium" | "large" | number
+  size: 'xSmall' | 'small' | 'medium' | 'large' | number;
+  notification?: boolean;
+  notificationColor?: ColorValue
 }
 
-const Avatar: FunctionComponent<AvatarProps> = ({ size, style, ...props }) => {
-  const passedProps = Array.isArray(props) ? Object.assign({}, ...props) : props
-  const passedStyles = Array.isArray(style) ? Object.assign({}, ...style) : style
-  const imgSize = size === "xSmall" ? 24 : size === "small" ? 48 : size === "medium" ? 72 : size === "large" ? 120 : size
+const Avatar: FunctionComponent<AvatarProps> = ({ notification, notificationColor, size, style, ...props }) => {
+  const passedProps = Array.isArray(props) ? Object.assign({}, ...props) : props;
+  const passedStyles = Array.isArray(style) ? Object.assign({}, ...style) : style;
+  const imgSize =
+    size === 'xSmall'
+      ? 24
+      : size === 'small'
+        ? 48
+        : size === 'medium'
+          ? 72
+          : size === 'large'
+            ? 120
+            : size;
+
   return (
-    <View>
-      <Image {...passedProps} style={{ width: imgSize, height: imgSize, borderRadius: imgSize / 2, ...passedStyles }} />
+    <View style={notification && {
+      padding: 4,
+      borderWidth: 2,
+      borderColor: notificationColor || '#000',
+      width: imgSize + 6,
+      height: imgSize + 6,
+      borderRadius: (imgSize + 6) / 2,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <Image
+        {...passedProps}
+        style={{ width: imgSize, height: imgSize, borderRadius: imgSize / 2, ...passedStyles }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
--- src/screens/AvatarScreen.js
+++ src/screens/AvatarScreen.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Avatar from '../components/Avatar';
 
 const AvatarScreen = () => {
   return (
     <View>
-      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size="xSmall" />
-      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size="xSmall" />
-      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size="small" />
-      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size="medium" />
+      <Avatar
+        source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
+        size="xSmall"
+        notification
+      />
+      <Avatar
+        source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
+        size="xSmall"
+        notification
+      />
+      <Avatar
+        source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
+        size="small"
+        notification
+      />
+      <Avatar
+        source={{ uri: 'https://ui-avatars.com/api/?background=random' }}
+        size="medium"
+        notification
+        notificationColor="blue"
+      />
       <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size="large" />
-      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size="30" />
+      <Avatar source={{ uri: 'https://ui-avatars.com/api/?background=random' }} size={30} />
     </View>
   );
 };
